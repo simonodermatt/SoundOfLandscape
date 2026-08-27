d5dd55a267892e7462174e371e8aa3f140dfd37e
Odermatt Simon <sp.odermatt@gmail.com>
2026-08-27 17:31:22 +0200

    adapt Map

---
+++ lang.js
@@ -0,0 +1,23 @@
+// lang.js - Hier liegen alle Übersetzungen
+const text = {
+    de: {
+        ausschnitt: "🌍 Ausschnitt:",
+        schweiz: "Schweiz",
+        europa: "Europa",
+        welt: "Welt",
+        kontur: "Panorama Kontur",
+        gipfel: "Gipfel (Melodie):",
+        taeler: "Täler (Bass):",
+        play: "🎵 Akkord abspielen"
+    },
+    en: {
+        ausschnitt: "🌍 View:",
+        schweiz: "Switzerland",
+        europa: "Europe",
+        welt: "World",
+        kontur: "Panorama Contour",
+        gipfel: "Peaks (Melody):",
+        taeler: "Valleys (Bass):",
+        play: "🎵 Play Chord"
+    }
+};
