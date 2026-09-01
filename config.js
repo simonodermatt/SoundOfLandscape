26c024e33107348190a3340585d2588cf76a64c0
Odermatt Simon <sp.odermatt@gmail.com>
2026-09-01 11:43:52 +0200

    Fix formatting in config.js

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
-
 const SHEET_ID = "10pxYaSMyt5uDRjCF0DWjGEabe_a3YftdpChxYzyQJBo"; 
 const API_URL = "https://script.google.com/macros/s/AKfycbyIx4JRbeLauQK4c41kW6sIwPByEqFRKHeEgTpCzdixe-vayEsNZ2h61H7_WTgASSbu/exec";
