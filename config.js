e3197690eb460b7a609c2f471356b1f8e3f48511
Odermatt Simon <sp.odermatt@gmail.com>
2026-09-01 19:55:33 +0200

    Update API URL in config.js and increment version in index.html

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 const SHEET_ID = "10pxYaSMyt5uDRjCF0DWjGEabe_a3YftdpChxYzyQJBo"; 
-const API_URL = "https://script.google.com/macros/s/AKfycbyN7jtPB29kaL7_5wBV342HqNvCwfbIA8v1mz1-zUPhJI-tCbfPPIb6uGfNuN83A4_I/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzoM2_7ENqvfI37Aqq930sNaZo3r9byGRWT9sredRnq1IEfyzyoIptDenmi_d8V_FHx/exec";
 
