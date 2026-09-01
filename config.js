f21285664af7481cd06bb0d036648a676607eca1
Odermatt Simon <sp.odermatt@gmail.com>
2026-09-01 19:16:25 +0200

    update API

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 const SHEET_ID = "10pxYaSMyt5uDRjCF0DWjGEabe_a3YftdpChxYzyQJBo"; 
-const API_URL = "https://script.google.com/macros/s/AKfycbyIx4JRbeLauQK4c41kW6sIwPByEqFRKHeEgTpCzdixe-vayEsNZ2h61H7_WTgASSbu/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwF7WNjiH6Na6zC0qOcgVm3lhIEXBxiYd55LPEdJBaog-s5iFgQ5ATqmxliNjCz4N_N/exec";
+
