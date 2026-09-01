33799c7203d31c9eaaf7945f338b927025884682
Odermatt Simon <sp.odermatt@gmail.com>
2026-09-01 11:33:54 +0200

    Update API_URL to new endpoint

--- engine.js
+++ engine.js
@@ -1,7 +1,7 @@
 // engine.js - Ausgelagerte Logik für den Panorama Synthesizer
 
 const SHEET_ID = "10pxYaSMyt5uDRjCF0DWjGEabe_a3YftdpChxYzyQJBo"; 
-const API_URL = "https://script.google.com/macros/s/AKfycbz08qUIVvAbiWibACoBOzRB9c5IDUgkylPFOGJfKvQyqItECg7WvlU9IEIQ5UBN0Sxg/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyIx4JRbeLauQK4c41kW6sIwPByEqFRKHeEgTpCzdixe-vayEsNZ2h61H7_WTgASSbu/exec";
 
 let currentLang = 'de';
 let panoramenDaten = [];
